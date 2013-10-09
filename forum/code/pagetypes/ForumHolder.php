6caa48fe5a94052ba6111d44f0a637369423f98d
Colby Klein <shakesoda@gmail.com>
2013-10-09 13:32:39 -0700

    Fix currently online user list

--- forum/code/pagetypes/ForumHolder.php
+++ forum/code/pagetypes/ForumHolder.php
@@ -224,11 +224,13 @@ class ForumHolder extends Page {
 			$groupIDs[] = $adminGroup->ID;
 		}
 
-		return Member::get()
+		$online = Member::get()
 			->leftJoin('Group_Members', 'Member.ID = Group_Members.MemberID')
 			->filter('GroupID', $groupIDs)
-			->filter("LastViewed:GreaterThan", DB::getConn()->datetimeIntervalClause('NOW', '-15 MINUTE'))
-			->sort('Member.FirstName, Member.Surname');
+			->filter("LastViewed:GreaterThan", date('Y-m-d H:i:s', time() - 900))
+			->sort('Member.Nickname, Member.FirstName, Member.Surname');
+
+		return $online;
 	}
 	
 	/**
