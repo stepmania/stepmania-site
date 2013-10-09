ca9ee3a7b0fda828ae39f48c5022effc1cbd0cd2
Colby Klein <shakesoda@gmail.com>
2013-10-09 13:33:34 -0700

    Associate a forum with HomePage to pick articles from

--- stepmania/code/HomePage.php
+++ stepmania/code/HomePage.php
@@ -1,19 +1,19 @@
 <?php
 class HomePage extends SiteTree {
-	public function getCMSFields() {
+	function getCMSFields() {
 		$fields = parent::getCMSFields();
 
 		$fields->addFieldToTab('Root.Link', new TreeDropdownField("NewsForumID", "Forum to pull content from", "SiteTree"));
 
 		return $fields;
 	}
-	public static $has_one = array(
+	static $has_one = array(
 		"NewsForum" => "Forum"
 	);
 }
 class HomePage_Controller extends Page_Controller {
-	public static $allowed_actions = array ();
-	public function init() {
+	static $allowed_actions = array ();
+	function init() {
 		parent::init();
 	}
 }
